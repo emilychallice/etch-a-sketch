a15a7fe57a73d51f2af5d1da66e0d9cc177f12b8
Emily Crawley <72890369+emilychallice@users.noreply.github.com>
2024-06-18 19:57:42 -0400

    Add ability to recreate gameboard with variable size (i.e. pixel density)
    By default starts with 16x16, will be allowed to vary up to 100x100.

--- script.js
+++ script.js
@@ -2,37 +2,53 @@ const gameboard = document.querySelector("#gameboard");
 const clearButton = document.querySelector("#clear-button");
 const rainbowButton = document.querySelector("#rainbow-button");
 
-const gameboardSize = 16;
-let gameboardPixels = [];
-
 let rainbowModeActive = 0;
 
+const initialGameboardSize = 16;
 
-// Give the gameboard 16x16 pixels
-for (let i = 0; i < gameboardSize**2; i++)
-{
-  let pixel = document.createElement("div");
+let gameboardPixels = createPixels(initialGameboardSize);
 
-  // Give pixels a common class and unique id for later
-  pixel.classList.add("gameboard-pixel");
-  pixel.id = "pixel" + i;
 
-  gameboard.appendChild(pixel);
-  gameboardPixels.push(pixel);
+function refreshGameboard(gameboardSize) {
+  removeAllPixels();
+  return createPixels(gameboardSize);
 }
 
+// Give the gameboard 16x16 pixels
+function createPixels(gameboardSize) {
+  let pixels = [];
+
+  for (let i = 0; i < gameboardSize ** 2; i++) {
+    let pixel = document.createElement("div");
+
+    // Give pixels a common class and unique id for later
+    pixel.classList.add("gameboard-pixel");
+    pixel.id = "pixel" + i;
+    pixel.style.width = 320/gameboardSize + "px";
+    pixel.style.height = 320/gameboardSize + "px";
+
+    gameboard.appendChild(pixel);
+    pixels.push(pixel);
+  }
+  return pixels;
+}
+
+function removeAllPixels() {
+  if (gameboardPixels.length > 0) {
+    gameboardPixels.forEach( (pixel) => {pixel.remove();} );
+  }
+}
 
 // Mouseover turns pixel a colour
 gameboard.addEventListener("mouseover", mouseoverHandler);
-function mouseoverHandler(e)
-{
-  let pixelMousedOver = document.querySelector("#" + e.target.id)
+function mouseoverHandler(e) {
+  let pixelMousedOver = document.querySelector("#" + e.target.id);
   let rgbR, rgbG, rgbB;
 
   if (rainbowModeActive) {
-    rgbR = Math.round( (Math.random() * 255) );
-    rgbG = Math.round( (Math.random() * 255) );
-    rgbB = Math.round( (Math.random() * 255) );
+    rgbR = Math.round(Math.random() * 255);
+    rgbG = Math.round(Math.random() * 255);
+    rgbB = Math.round(Math.random() * 255);
   } else {
     rgbR = 0;
     rgbG = 0;
@@ -41,28 +57,21 @@ function mouseoverHandler(e)
   pixelMousedOver.style.backgroundColor = `rgb(${rgbR}, ${rgbG}, ${rgbB})`;
 }
 
-
 // Clear button
 clearButton.onclick = clearGameBoard;
-function clearGameBoard()
-{
-  gameboardPixels.forEach( (pix) => {
+function clearGameBoard() {
+  gameboardPixels.forEach((pix) => {
     pix.style.backgroundColor = "white";
   });
 }
 
-
 // Rainbow button
 rainbowButton.onclick = rainbowButtonClicked;
-function rainbowButtonClicked()
-{
-  if (rainbowModeActive)
-  {
+function rainbowButtonClicked() {
+  if (rainbowModeActive) {
     rainbowModeActive = 0;
     rainbowButton.textContent = "Rainbow";
-  }
-  else
-  {
+  } else {
     rainbowModeActive = 1;
     rainbowButton.textContent = "Monochrome";
   }
